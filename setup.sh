7302cf0527bb15a975d64b9365cdcd052145c4be
Jon Van Steen <Jon.VanSteen@inin.com>
2016-10-27 11:34:23 -0400

    making some tweaks

--- setup.sh
+++ setup.sh
@@ -3,15 +3,19 @@
 #Install brew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+echo "Installing some brew things..."
 #Install some brew things
 brew install git nodejs ruby cask zsh wget python3 chromedriver
 
+echo "Installing some casks..."
 #Install some casks
 brew cask install java google-chrome firefox postman sublime-text atom wireshark vlc spectacle keepingyouawake licecap tunnelblick iterm2
 
+echo "Installing maven"
 #maven requires java so needed to install that first
 brew install maven
 
+echo 'Lets install some other things, make sure to download them!'
 #Need to install some other things
 urls=(
     'https://www.jetbrains.com/idea/download/'
@@ -21,6 +25,7 @@ do
     open "${var}"
 done
 
+echo "Time to install oh-my-zsh"
 #install oh-my-zsh
 if which zsh > /dev/null;
 then
@@ -32,3 +37,10 @@ then
 else
   echo "Zsh is not installed. Go do that and then install oh-my-zsh"
 fi
+
+#setup ssh key
+echo "Lets setup an ssh key"
+ssh-keygen -t rsa
+
+#all done
+echo "All done!"
